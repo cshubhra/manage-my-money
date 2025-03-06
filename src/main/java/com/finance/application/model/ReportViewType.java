226e59f0de1863eb8412b98e66fbb441d3e226dd
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-03-06 11:01:45 -0500

    Add ReportViewType enum

---
+++ src/main/java/com/finance/application/model/ReportViewType.java
@@ -0,0 +1,17 @@
+package com.finance.application.model;
+
+/**
+ * Enum representing different report view types.
+ * This corresponds to the original 'report_view_type' in the Ruby on Rails application.
+ */
+public enum ReportViewType {
+    /**
+     * Text view type displays the report as text.
+     */
+    TEXT,
+    
+    /**
+     * Graph view type displays the report as a graph.
+     */
+    GRAPH
+}
