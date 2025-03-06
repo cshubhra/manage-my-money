ba9be5f2d3cc064d8019f8b008e2915006a3d62c
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-03-06 10:58:39 -0500

    Add TransferItemType enum

---
+++ src/main/java/com/finance/application/model/TransferItemType.java
@@ -0,0 +1,16 @@
+package com.finance.application.model;
+
+/**
+ * Enum representing the type of a transfer item.
+ */
+public enum TransferItemType {
+    /**
+     * Income type represents money coming in (positive value).
+     */
+    INCOME,
+    
+    /**
+     * Outcome type represents money going out (negative value).
+     */
+    OUTCOME
+}
