a16e888693804f48bdd88218dfa585384f1ec2a5
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-03-06 12:17:32 -0500

    Create User Controller

---
+++ backend/src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.registerUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok("Login functionality not implemented yet.");
+    }
+}
