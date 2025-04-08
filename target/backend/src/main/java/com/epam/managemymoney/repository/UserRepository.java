6dc759db2b712f3a029d236393a3202a428b9965
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-04-08 17:19:28 -0400

    refactor: create UserRepository in proper package

---
+++ target/backend/src/main/java/com/epam/managemymoney/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.epam.managemymoney.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.epam.managemymoney.model.User;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
