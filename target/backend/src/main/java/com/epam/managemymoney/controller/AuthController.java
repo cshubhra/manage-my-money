c9b21f8abd69c7e7d320255a466664ecf1ed69da
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-04-08 17:08:51 -0400

    refactor: move AuthController to com.epam.managemymoney.controller package

---
+++ target/backend/src/main/java/com/epam/managemymoney/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.epam.managemymoney.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import com.epam.managemymoney.service.AuthenticationService;
+import com.epam.managemymoney.dto.AuthRequestDTO;
+import com.epam.managemymoney.dto.AuthResponseDTO;
+
+@RestController
+@RequestMapping("/api/auth")
+@Slf4j
+public class AuthController {
+    private final AuthenticationService authService;
+
+    @Autowired
+    public AuthController(AuthenticationService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody AuthRequestDTO request) {
+        log.info("Authentication request received for user: {}", request.getUsername());
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@Valid @RequestBody AuthRequestDTO request) {
+        log.info("Registration request received for user: {}", request.getUsername());
+        return ResponseEntity.ok(authService.register(request));
+    }
+}
