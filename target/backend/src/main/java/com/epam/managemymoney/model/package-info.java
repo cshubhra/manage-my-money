64cf326311f55e15726aead66c3a80bf8567c0b8
cshubhra <shubhrajit.chatterjee@gmail.com>
2025-04-09 12:41:53 -0400

    Initialize model package

---
+++ target/backend/src/main/java/com/epam/managemymoney/model/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains all domain model classes for the Manage My Money application.
+ * These classes represent the core business entities and their relationships.
+ */
+package com.epam.managemymoney.model;
