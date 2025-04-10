753ec79519cf691ef5ac15d5e061991215faaf98
Shubhrajit Chatterjee <shubhrajit_chatterjee@epam.com>
2025-04-10 11:42:28 -0400

    Manual fix

--- target/backend/src/main/java/com/epam/managemymoney/repository/GoalRepository.java
+++ target/backend/src/main/java/com/epam/managemymoney/repository/GoalRepository.java
@@ -4,10 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.epam.managemymoney.model.Goal;
 import com.epam.managemymoney.model.GoalStatus;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
     List<Goal> findByUserId(Long userId);
     List<Goal> findByUserIdAndStatus(Long userId, GoalStatus status);
+
+    List<Goal> findByUserIdAndStatusNot(Long userId, String status);
+    List<Goal> findByUserIdAndPriorityOrderByTargetDateAsc(Long userId, String priority);
+    List<Goal> findByUserIdAndTargetDateGreaterThanAndStatusNotOrderByTargetDateAsc(
+            Long userId, LocalDate date, String status);
 }
